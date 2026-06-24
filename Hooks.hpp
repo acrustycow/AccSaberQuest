1a85e4e06209f74ab9d4cf09dd78a1bbc692a97f
acrustycow <pannellcyle@gmail.com>
2026-06-24 08:15:12 -0500

    Update Hooks.hpp

--- Hooks.hpp
+++ Hooks.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
-namespace AccSaberQuest::Hooks {
-    void InstallHooks();
-}
+#include "beatsaber-hook/shared/utils/logging.hpp"
+
+// Logger used across the whole mod
+static constexpr auto MOD_LOGGER = Paper::ConstLoggerContext("AccSaberQuest");
+
+#define LOG_INFO(...)  MOD_LOGGER.info(__VA_ARGS__)
+#define LOG_WARN(...)  MOD_LOGGER.warn(__VA_ARGS__)
+#define LOG_ERROR(...) MOD_LOGGER.error(__VA_ARGS__)
+#define LOG_DEBUG(...) MOD_LOGGER.debug(__VA_ARGS__)
